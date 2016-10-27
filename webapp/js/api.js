a6653128ecfbb05a18e68a5a5e7301f400340892
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2016-10-27 09:28:52 +0200

    Api: add methods for basic CRUD operations

--- webapp/js/api.js
+++ webapp/js/api.js
@@ -14,12 +14,73 @@ class Api
 
     getNamespaces() {
         return fetch(this.url+'/dataStore', this.getHeaders())
+            .then(response => this.successOnly(response))
             .then(response => response.json());
+    }
+
+    deleteNamespace(namespace) {
+        return fetch(this.url+'/dataStore/'+namespace, Object.assign({}, {
+            'method': 'DELETE',
+        }, this.getHeaders()))
+            .then(response => this.successOnly(response))
+            .then(response => response.json());
+    }
+
+    getKeys(namespace) {
+        return fetch(this.url+'/dataStore/'+namespace, this.getHeaders())
+            .then(response => this.successOnly(response))
+            .then(response => response.json());
+    }
+
+    getValue(namespace, key) {
+        return fetch(this.url+'/dataStore/'+namespace+'/'+key, this.getHeaders())
+            .then(response => this.successOnly(response))
+            .then(response => response.json());
+    }
+
+    getMetaData(namespace, key) {
+        return fetch(this.url+'/dataStore/'+namespace+'/'+key+'/metaData', this.getHeaders())
+            .then(response => this.successOnly(response))
+            .then(response => response.json());
+    }
+
+    createValue(namespace, key, value) {
+        return fetch(this.url+'/dataStore/'+namespace+'/'+key, Object.assign({}, {
+            'method': 'POST',
+            'body': JSON.stringify(value),
+        }, this.getHeaders()))
+            .then(response => this.successOnly(response))
+            .then(response => response.json());
+    }
+
+    updateValue(namespace, key, value) {
+        return fetch(this.url+'/dataStore/'+namespace+'/'+key, Object.assign({}, {
+            'method': 'PUT',
+            'body': JSON.stringify(value),
+        }, this.getHeaders()))
+            .then(response => this.successOnly(response))
+            .then(response => response.json());
+    }
+
+    deleteValue(namespace, key) {
+        return fetch(this.url+'/dataStore/'+namespace+'/'+key, Object.assign({}, {
+            'method': 'DELETE',
+        }, this.getHeaders()))
+            .then(response => this.successOnly(response))
+            .then(response => response.json());
+    }
+
+    successOnly(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return Promise.resolve(response);
+        }
+        return Promise.reject(response);
     }
 
     getHeaders() {
         return {
             'headers': {
+                'Content-Type': 'application/json',
                 'Authorization': this.auth
             }
         };
