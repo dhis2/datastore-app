eb938cef222c57daefc05935c5fbfbc57c28a84b
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2016-11-27 18:36:40 +0100

    breadcrumbs: last breadcrumb doesn’t render chevron

--- webapp/js/components/window/WindowToolbarTitle.js
+++ webapp/js/components/window/WindowToolbarTitle.js
@@ -5,11 +5,12 @@ import BreadcrumbHistoryButton from '../utils/BreadcrumbHistoryButton';
 class WindowToolbarTitle extends Component {
     renderPath(path) {
         const pathArray = path.split('/');
+        const length = pathArray.length-1;
 
         return (
       <span className={'toolbar-title'}>
         {pathArray.map((item, index) => (
-           <BreadcrumbHistoryButton label={ item } key={ index } />
+           <BreadcrumbHistoryButton label={ item } key={ index } last={index === length} />
           ))}
       </span>
         );
