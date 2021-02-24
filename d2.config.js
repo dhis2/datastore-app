1224c01adb291b2b255b0dd4d1a74a1f1252d060
Médi-Rémi Hashim <mediremi@gmail.com>
2021-02-24 12:52:09 +0000

    chore: create new d2 app

---
+++ d2.config.js
@@ -0,0 +1,12 @@
+const config = {
+    type: 'app',
+    name: 'datastore',
+    title: 'Datastore Management',
+    coreApp: true,
+
+    entryPoints: {
+        app: './src/App',
+    },
+}
+
+module.exports = config
--- webapp/js/actions/navigationActions.js
+++
@@ -1,8 +0,0 @@
-import { SET_IGNORE_NEXT_CONFIRM_NAVIGATION } from '../constants/actionTypes';
-
-export function setIgnoreNextNavigationConfirm(ignoreNext = true) {
-    return {
-        type: SET_IGNORE_NEXT_CONFIRM_NAVIGATION,
-        ignoreNextConfirm: ignoreNext
-    };
-}
