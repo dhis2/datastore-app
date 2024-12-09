3c84980808b1734e5a55fcefeb2fbd16d927c5c8
Diana Nanyanzi <diana@dhis2.org>
2024-12-09 06:08:29 +0300

    feat: add error element

--- src/components/pager/routes/PagerRouter.tsx
+++ src/components/pager/routes/PagerRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navigate, createHashRouter } from 'react-router-dom'
 import EditPage from '../pages/Edit'
+import ErrorComponent from '../pages/Error'
 import KeysPage from '../pages/Keys'
 import NamespacesPage from '../pages/Namespaces'
 import PagerLayout from './PagerLayout'
@@ -9,6 +10,7 @@ export const pagerRouter = createHashRouter([
     {
         path: '/',
         element: <PagerLayout />,
+        errorElement: <ErrorComponent />,
         children: [
             { index: true, element: <Navigate to={'dataStore'} replace /> },
             {
