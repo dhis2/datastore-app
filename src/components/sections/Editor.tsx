d60b78d1ad9e463476c15b385460e503f31385cd
Diana Nanyanzi <diana@dhis2.org>
2024-12-18 16:30:08 +0300

    feat: add ui components for the edit page panels

---
+++ src/components/sections/Editor.tsx
@@ -0,0 +1,22 @@
+import { json } from '@codemirror/lang-json'
+import CodeMirror, { ViewUpdate } from '@uiw/react-codemirror'
+import React from 'react'
+
+type EditorProps = {
+    handleChange?: (value: string, viewUpdate: ViewUpdate) => void
+    value: string
+}
+
+const Editor = ({ value, handleChange }: EditorProps) => {
+    return (
+        <CodeMirror
+            theme={'dark'}
+            value={value}
+            height="83vh"
+            extensions={[json()]}
+            onChange={handleChange}
+        />
+    )
+}
+
+export default Editor
