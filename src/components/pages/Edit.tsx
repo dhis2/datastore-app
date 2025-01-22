4a824b03183d7f7a0ad9b96a24ae6ebf9d7c788a
Joe Cooper <33054985+cooper-joe@users.noreply.github.com>
2025-01-22 13:06:27 +0100

    feat: layout adjustments (#143)
    * fix: adjust css styles
    
    * fix: adjust layout
    
    * fix: minor adjustments
    
    - fix the height of the keys and editor panel components
    - remove unused components
    
    ---------
    
    Co-authored-by: Diana Nanyanzi <diana@dhis2.org>

--- src/components/pages/Edit.tsx
+++ src/components/pages/Edit.tsx
@@ -1,23 +1,11 @@
-import { IconArrowLeft16 } from '@dhis2/ui'
 import React from 'react'
-import { Link, Outlet, useParams } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import classes from '../../App.module.css'
-import i18n from '../../locales'
-import PageHeader from '../header/PageHeader'
 import KeysPanel from '../panels/KeysPanel'
 
 const EditPage = () => {
-    const { store } = useParams()
-
     return (
         <div className={classes.secondPage}>
-            <PageHeader>
-                <Link to={`/${store}`} className={classes.secondPageHeader}>
-                    <span>
-                        <IconArrowLeft16 /> {i18n.t('Back to all namespaces')}
-                    </span>
-                </Link>
-            </PageHeader>
             <div className={classes.secondPageContainer}>
                 <div className={classes.keysPanel}>
                     <KeysPanel />
--- src/components/table/ItemsTable.tsx
+++ src/components/table/ItemsTable.tsx
@@ -58,6 +58,7 @@ const ItemsTable = ({ data, label }: TableProps) => {
                                         >
                                             <DataTableCell
                                                 bordered
+                                                large={!currentNamespace}
                                                 width={
                                                     currentNamespace
                                                         ? '85%'
