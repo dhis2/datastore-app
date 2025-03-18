ce4206ec46af0240dd187a8b7ac196c244f63ab1
Diana Nanyanzi <diana@dhis2.org>
2025-03-18 19:40:28 +0300

    chore: add minDHIS2 version and app title

--- d2.config.js
+++ d2.config.js
@@ -1,7 +1,10 @@
 const config = {
     type: 'app',
     name: 'datastore',
+    title: 'Datastore',
     id: 'c968eb49-01f5-4904-b4d3-f3785c6bfc09',
+
+    minDHIS2Version: '2.41',
 
     entryPoints: {
         app: './src/App.tsx',
