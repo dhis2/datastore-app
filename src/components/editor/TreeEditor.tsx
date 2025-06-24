5a968cbfbd4d9617a266c83cba44417b0f591067
Diana Nanyanzi <diana@dhis2.org>
2025-06-24 10:42:37 +0300

    feat: persist new key-value pairs across both editors

--- src/components/editor/TreeEditor.tsx
+++ src/components/editor/TreeEditor.tsx
@@ -4,61 +4,12 @@ import JsonViewEditor from '@uiw/react-json-view/editor'
 import React from 'react'
 import useCustomAlert from '../../hooks/useCustomAlert'
 import i18n from '../../locales'
+import {
+    findPath,
+    retrieveSelectedValue,
+    treeEditorStyle,
+} from '../../utils/treeEditorUtils'
 import ErrorNotice from '../error/ErrorNotice'
-
-const customTheme = {
-    '--w-rjv-color': '#e16c73',
-    '--w-rjv-key-number': '#818499',
-    '--w-rjv-key-string': '#e16c73', // root, key,
-    '--w-rjv-background-color': '#292c34',
-    '--w-rjv-line-color': 'transparent', // line colour
-    '--w-rjv-arrow-color': '#818499',
-    '--w-rjv-edit-color': '',
-    '--w-rjv-info-color': '#818499',
-    '--w-rjv-update-color': '',
-    '--w-rjv-copied-color': '',
-    '--w-rjv-copied-success-color': '',
-    '--w-rjv-curlybraces-color': '#acb4be',
-    '--w-rjv-colon-color': '#acb4be',
-    '--w-rjv-brackets-color': '#acb4be',
-    '--w-rjv-ellipsis-color': '',
-    '--w-rjv-quotes-color': '#eb6383',
-    '--w-rjv-quotes-string-color': '#9bc181',
-    // different data types - values
-    '--w-rjv-type-string-color': '#9cba7d',
-    '--w-rjv-type-int-color': '#e4c07c',
-    '--w-rjv-type-float-color': '#e4c07c',
-    '--w-rjv-type-bigint-color': '#e4c07c',
-    '--w-rjv-type-boolean-color': '#d9985f',
-    '--w-rjv-type-date-color': '#9cba7d',
-    '--w-rjv-type-url-color': '#9cba7d',
-    '--w-rjv-type-null-color': '#9cba7d',
-    '--w-rjv-type-nan-color': '#9cba7d',
-    '--w-rjv-type-undefined-color': '#9cba7d',
-}
-
-const treeEditorStyle = {
-    ...customTheme,
-    fontSize: '17px',
-    fontFamily:
-        "ui-monospace, Menlo, Monaco, 'Cascadia Mono', 'Segoe UI Mono','Roboto Mono', 'Oxygen Mono', 'Ubuntu Mono', 'Source Code Pro','Fira Mono', 'Droid Sans Mono', 'Consolas','Courier New', monospace",
-}
-
-const retrieveSelectedValue = ({ mainObj, path }) => {
-    let selectedValue = mainObj
-    const lastKey = path[path.length - 1]
-
-    if (path.length > 0) {
-        for (let i = 0; i < path.length - 1; i++) {
-            selectedValue = selectedValue[path[i]]
-        }
-    }
-
-    return {
-        selectedValue,
-        lastKey,
-    }
-}
 
 const TreeViewEditor = ({
     value: treeEditorValue,
@@ -132,6 +83,40 @@ const TreeViewEditor = ({
         }
     }
 
+    const handleAdd = (keyOrValue, newValue, value, isAdd) => {
+        const path = findPath(treeEditorValue, value)
+
+        if (path === null) {
+            return
+        }
+
+        // using retrieve selected value
+        const { selectedValue, lastKey } = retrieveSelectedValue({
+            mainObj: treeEditorValue,
+            path: path,
+        })
+
+        if (Array.isArray(newValue)) {
+            const index = newValue.findIndex((el) => el === keyOrValue)
+            newValue[index] = 'value'
+        } else {
+            newValue[keyOrValue] = 'value'
+        }
+
+        if (lastKey !== null) {
+            selectedValue[lastKey] = newValue
+        } else {
+            if (Array.isArray(selectedValue)) {
+                selectedValue.push('value')
+            } else {
+                selectedValue[keyOrValue] = 'value'
+            }
+        }
+
+        onChange?.(JSON.stringify(treeEditorValue, null, 4))
+        return isAdd
+    }
+
     return error ? (
         <ErrorNotice
             message={i18n.t(
@@ -156,7 +141,7 @@ const TreeViewEditor = ({
                 editable={!loading}
                 onDelete={handleDelete}
                 onEdit={handleEdit}
-                onAdd={(_kv, _nv, _v, isAdd) => isAdd}
+                onAdd={handleAdd}
             />
         </div>
     )
