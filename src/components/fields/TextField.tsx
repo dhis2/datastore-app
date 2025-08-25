8299e76c3fd92e1f96403ce2c0e11b37918fc3c7
Diana Nanyanzi <diana@dhis2.org>
2025-08-25 16:06:40 +0200

    feat: validate against url unsafe characters in new key and namespace names

--- src/components/fields/TextField.tsx
+++ src/components/fields/TextField.tsx
@@ -4,10 +4,17 @@ import {
     hasValue,
     composeValidators,
     createMaxCharacterLength,
+    createPattern
 } from '@dhis2/ui'
 import React from 'react'
 
 const { Field } = ReactFinalForm
+
+// source: https://stackoverflow.com/questions/1547899/which-characters-make-a-url-invalid/13500078#13500078
+const invalidCharactersRegex = /^[^(){}[\]^|`;?:@=+$,\\]+$/ 
+
+const invalidCharactersMessage = "Your input should not contain any of these invalid characters: {}|\\^[]`;?:@=+$,"
+const validateInputCharacters = createPattern(invalidCharactersRegex, invalidCharactersMessage)
 
 const TextField = ({
     initialFocus,
@@ -28,7 +35,8 @@ const TextField = ({
             label={label}
             validate={composeValidators(
                 hasValue,
-                createMaxCharacterLength(255)
+                createMaxCharacterLength(255),
+                validateInputCharacters
             )}
             initialFocus={initialFocus}
         />
